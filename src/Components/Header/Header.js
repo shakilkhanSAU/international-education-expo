66cfe6a3fe8366509e8ef84feba28dbaaf86aa85
shakilkhanSAU <shakilkhan.sau@gmail.com>
2021-09-26 19:50:18 +0600

    header fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <div className="header">
             <img className="img-fluid" src={logo} alt="logo" />
             <h1 className="heading">International Education Expo 2021</h1>
+            <p className="header-message fw-bold"><small>Invite World Class Teacher for Upcomming Education Conference</small></p>
             <p className="fs-4">Total Budget: <span className="fw-bold">100 Milion US Dollars</span></p>
             <nav>
                 <a href="/home">Home</a>
